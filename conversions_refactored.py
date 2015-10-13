073dcddef86b687176c247cd730c1e4992a13836
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-10-13 02:07:51 +0000

    attempting to refactor

---
+++ conversions_refactored.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A refactoring module."""
+
+##Author - Erica Liz
+
+import unittest
+
+class ConversionNotPossible:(Exception):
+    """Conversions from compatible units."""
+    pass
+
+class TestConversions(unittest.TestCase):
+    """Tests for refactored conversions."""
+    values = #need some values
+
+
+
+
